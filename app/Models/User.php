36671ca9eea59945eb58826993b024e609c063fa
hellomuzi <youyunchuxiu@163.com>
2017-03-29 05:43:10 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,16 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+
+    /**
+     * Gravatar 头像
+     * @param  string $size [description]
+     * @return [type]       [description]
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
