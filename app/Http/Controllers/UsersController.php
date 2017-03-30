a0324d1f687ba597b7b65551302cf50f4d776309
hellomuzi <youyunchuxiu@163.com>
2017-03-30 09:19:12 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,17 +15,38 @@ use Auth;
  */
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        // 用中间件过滤未登陆用户的行为
+        $this->middleware('auth', [
+            'only' => ['edit', 'update', 'show', 'index', 'destroy']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    // 所有用户信息展示
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+    // 注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    // 个人信息
     public function show($id)
     {
         $user = User::findOrFail($id);
         return view('users.show', compact('user'));
     }
 
+    // 处理注册提交信息
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -43,5 +64,46 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    // 编辑页面
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    // 处理更新信息
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'confirmed|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $id);
+    }
+
+    // 删除
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户');
+        return back();
     }
 }
